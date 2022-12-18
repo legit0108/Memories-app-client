f717436921564e6195857a19231a934b6973a99e
legit0108 <legitgamer2345@gmail.com>
2022-12-18 18:08:28 +0530

    Testing forgot password

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {TextField, Avatar, Button, Paper, Grid, Typography, Container} from '@material-ui/core'
 import useStyles from './styles'
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
